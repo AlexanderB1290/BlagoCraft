bb2d587c22cb340f4b5073df0bd1929cab65586c
Aleksandar Bushev <alexander.bushev@gmail.com>
2021-02-11 20:22:45 +0200

    BlagoCraft server backup 02/11/2021

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ time_stamp=$(date +'%m/%d/%Y')
 git commit -a -m "BlagoCraft server backup ${time_stamp}"
 git push https://AlexanderB1290:Go2hell#GH451@github.com/AlexanderB1290/BlagoCraft.git
 
-java -Xms512M -Xmx4G -XX:+UseG1GC -jar spigot-server.jar nogui
+#java -Xms512M -Xmx4G -XX:+UseG1GC -jar spigot-server.jar nogui
